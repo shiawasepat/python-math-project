325cdc1acf8f5f82da89fc1b3afbd6b873802207
Patrick <shiawasearuji@gmail.com>
2025-01-20 09:36:39 +0800

    init ulang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from features.calculate_sum import calculate_sum #dev 1
+from features.calculate_division import calculate_division #dev 2
+from features.calculate_product import calculate_product #dev 2
 from features.calculate_difference import calculate_difference #dev 2
 
 a = 10
@@ -7,4 +9,3 @@ sum_result = calculate_sum(a,b)
 print(f"Sum: {sum_result}") 
 
 print(calculate_difference(10, 5))
-main
